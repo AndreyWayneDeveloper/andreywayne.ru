6f08fdb2d9b1d1642a750bb050afcfd978a89331
AndreyWayneDeveloper <developer@andreywayne.ru>
2019-05-26 03:52:20 +0300

    new site

--- plugins/Container.js
+++
@@ -1,9 +0,0 @@
-import styled, { css } from 'vue-styled-components'
-import Vue from 'vue'
-import style from 'vue-styled-components'
-
-export default styled.div`
-  position: relative;
-  width: 1200px;
-  margin: 0 auto;
-`
